8d29993fc932e96d449d5d4b54c4fa0ed33cb5c6
JackPaul <JackPaul@MAIN-Computer>
2020-04-06 01:20:21 -0600

    Reformated Help.help()

--- Crypt/src/actions/Help.java
+++ Crypt/src/actions/Help.java
@@ -4,12 +4,14 @@ public class Help
 {
 	public static void help()
 	{
+		System.out.println("");
 		System.out.println("The syntax of the command is as follows:");
-		System.out.println("Crypt [/action] {args[]}");
-		System.out.println("/e - Encrypts files, crypt /e [path] [keyName] [password]");
-		System.out.println("/d - Decrypts files, crypt /d [path] [keyName] [password]");
-		System.out.println("/g - Generates key, crypt /g [name] [password] [size]");
-		System.out.println("/r - Removes key, crypt /r [name]");
-		System.out.println("*CRYPT_KEYS enviroment variable must be set*");
+		System.out.println("   Crypt [/action] {args[]}");
+		System.out.println("   /e - Encrypts files, crypt /e [path] [keyName] [password]");
+		System.out.println("   /d - Decrypts files, crypt /d [path] [keyName] [password]");
+		System.out.println("   /g - Generates key, crypt /g [name] [password] [size]");
+		System.out.println("   /r - Removes key, crypt /r [name]");
+		System.out.println("");
+		//System.out.println("*CRYPT_KEYS enviroment variable must be set*");
 	}
 }
