1cafa26a53382a3d360c9ed1d33df2080eb541c9
JackPaul <JackPaul@MAIN-Computer>
2020-04-10 00:04:49 -0600

    Deleted depretiated code

--- Crypt/src/resources/Data.java
+++ Crypt/src/resources/Data.java
@@ -3,5 +3,4 @@ package resources;
 public class Data
 {
 	public static final String LOC = System.getenv("CRYPT_KEYS") + "\\";
-	public static final String TMP = "C:\\Program Files\\Crypt\\temp\\";
 }
